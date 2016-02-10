eb4e07811f8cfcce03a53c94f39cfe4b4787229f
Vladimir Ivashkin <illusionww@gmail.com>
2016-02-11 00:12:56 +0300

    linkedin parser done

--- linkedin-parser/parser.py
+++ linkedin-parser/parser.py
@@ -8,64 +8,72 @@ class LinkedinSearch(object):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
         }
 
-    def find_list_by_name(self, first_name, last_name):
+    def get_persons_by_name(self, first_name, last_name):
         url = 'http://www.linkedin.com/pub/dir/' + first_name + '/' + last_name + '/'
         page = requests.get(url, headers=self.headers).text
         soup = BeautifulSoup(page, "html.parser")
-        for profile in soup.find_all('div', 'profile-card'):
-            link = profile.find("div", "content").h3.a['href']
-            self.get_all_info_by_link(link)
+        return [self.get_person_info_by_link(profile.find("div", "content").h3.a['href']) for profile in soup.find_all('div', 'profile-card')]
 
-    def get_all_info_by_link(self, url):
+    def get_person_info_by_link(self, url):
         page = requests.get(url, headers=self.headers).text
         soup = BeautifulSoup(page, "html.parser")
 
         # SECTION: topcard
-        demographics = soup.find(id="demographics")
-        person = {'name': soup.find(id="name").text,
-                  'picture_url': soup.find("div", "profile-picture").a.img['data-delayed-url'],
+        section_topcard = soup.find(id="topcard")
+        profile_card = section_topcard.find("div", "profile-card")
+        profile_overview_content = profile_card.find("div", "profile-overview-content")
+        demographics = profile_overview_content.find(id="demographics")
+        person = {'name': profile_overview_content.find(id="name").text,
+                  'headline': profile_overview_content.find("p", "headline").text,
                   'locality': demographics.find("span", "locality").text,
                   'industry': demographics.find_all("dd", "descriptor")[1].text,
-                  'member_connections': soup.find("div", "member-connections").strong.text}
+                  'member_connections': profile_overview_content.find("div", "member-connections").strong.text}
+
+        if "no-picture" not in profile_card["class"]:
+            person['picture_url'] = profile_card.find("div", "profile-picture").a.img['data-delayed-url']
 
         # SECTION: experience
         experience_section = soup.find(id="experience")
-        person['positions'] = [{'position': position.find("h4", "item-title").a.text,
-                                       'organization': position.find("h5", "item-subtitle").a.text,
-                                        'data-range': position.find("span", "date-range").text
-                                        } for position in experience_section.find_all("li", "position")]
+        if experience_section:
+            person['positions'] = [{'position': position.find("h4", "item-title").a.text,
+                                    'organization': position.find("h5", "item-subtitle").a.text if position.find("h5", "item-subtitle").a else None,
+                                    'data-range': position.find("span", "date-range").text
+                                    } for position in experience_section.find_all("li", "position")]
 
         # SECTION: volunteering
         # skip
 
         # SECTION: languages
         languages_section = soup.find(id="languages")
-        person['languages'] = [{'name': language.find("h4", "name").text,
-                                'proficiency': language.find("p", "proficiency").text
-                                } for language in languages_section.find_all("li", "language")]
+        if languages_section:
+            person['languages'] = [{'name': language.find("h4", "name").text,
+                                    'proficiency': language.find("p", "proficiency").text
+                                    } for language in languages_section.find_all("li", "language")]
         # SECTION: projects
         # skip
 
         # SECTION: skills
         skills_section = soup.find(id="skills")
-        person['skills'] = [skill.a["title"] for skill in skills_section.ul.findChildren("li", "skill") if not any(x in skill["class"] for x in ["see-less", "see-more"])]
+        if skills_section:
+            person['skills'] = [skill.a["title"] for skill in skills_section.ul.find_all("li", "skill") if
+                                not any(x in skill["class"] for x in ["see-less", "see-more"])]
 
         # SECTION: education
         education_section = soup.find(id="education")
-        person['educations'] = [{"university": education.find("h4", "item-title").text,
-                                 "degree": education.find("h5", "item-subtitle").text,
-                                 "data-range": education.find("span", "date-range").text
-                                 } for education in education_section.findChildren("li", "school")]
-
-        print(person)
+        if education_section:
+            person['educations'] = [{"university": education.find("h4", "item-title").text,
+                                     "degree": education.find("h5", "item-subtitle").text,
+                                     "data-range": education.find("span", "date-range").text
+                                     } for education in education_section.find_all("li", "school")]
+        return person
 
 
 def main():
     first_name, last_name = "Vladimir Ivashkin".split()
 
     linkedin_search = LinkedinSearch()
-    linkedin_search.find_list_by_name(first_name, last_name)
-
+    persons = linkedin_search.get_persons_by_name(first_name, last_name)
+    print(persons)
 
 if __name__ == '__main__':
     main()
